75a520c80b78f8b63e8dc8e39f85f0d198cbd6bf
nurjaman10 <nurjaman1020@gmail.com>
2022-12-27 15:43:04 +0700

    baru

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+@include('layouts.header')
+  <!-- Preloader 
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div>-->
+
+  <!-- Navbar -->
+@include('layouts.navbar')
+  <!-- /.navbar -->
+@include('layouts.sidebar')
+  <!-- Content Wrapper. Contains page content -->
+@include('layouts.content')
+  <!-- /.content-wrapper -->
+@include('layouts.footer')
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,212 @@
+
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+        <img src="{{asset('AdminLTE-3.2.0')}}/dist/img/ponorogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Desa Purwosari</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+            <img src="{{asset('AdminLTE-3.2.0')}}/dist/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+            <a href="#" class="d-block">Administrator</a>
+        </div>
+    </div>
+    <!-- Sidebar Menu -->
+     <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+            with font-awesome or any other icon font library -->
+            <li class="nav-item menu-open">
+                <a href="#" class="nav-link active">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Dashboard
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon far fa-address-book"></i>
+                <p>
+                    Data Kependudukan
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/tables/simple.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Data KTP</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/tables/data.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Data Kartu Keluarga</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/tables/jsgrid.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Data BPJS</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-header">DATA DUKUH</li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon far fa-folder-open"></i>
+                <p>
+                    Dukuh Tempel
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/search/simple.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/search/enhanced.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 2</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="pages/gallery.html" class="nav-link">
+                <i class="nav-icon far fa-folder-open"></i>
+                <p>
+                    Dukuh Ngimput
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/search/simple.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/search/enhanced.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 2</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="pages/gallery.html" class="nav-link">
+                <i class="nav-icon far fa-folder-open"></i>
+                <p>
+                    Dukuh Cepet Utara
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/search/simple.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/search/enhanced.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 2</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="pages/gallery.html" class="nav-link">
+                <i class="nav-icon far fa-folder-open"></i>
+                <p>
+                    Dukuh Cepet Selatan
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/search/simple.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/search/enhanced.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 2</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="pages/gallery.html" class="nav-link">
+                <i class="nav-icon far fa-folder-open"></i>
+                <p>
+                    Dukuh Kajang
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/search/simple.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/search/enhanced.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DATA RT 2</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-header">SETTINGS</li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-cog"></i>
+                <p>
+                    Master
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/search/simple.html" class="nav-link">
+                        <i class="fas fa-bullhorn nav-icon"></i>
+                        <p>Data Berita</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/search/enhanced.html" class="nav-link">
+                        <i class="fas fa-users nav-icon"></i>
+                        <p>Data Users</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item menu-open">
+                <a href="#" class="nav-link active">
+                <i class="nav-icon fas fa-power-off"></i>
+                    </p>Logout</p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar -->
+</aside>
